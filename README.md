939cc0fa65d5e3de1091c26272030e6592758e40
yoojinc15 <54961282+yoojinc15@users.noreply.github.com>
2020-11-20 18:57:01 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# homework6
-539 Media player basic
+# homework_6
+Starter code for Homework 6 - Homework description can be found at: https://docs.google.com/document/d/1G7BvRqTUewblgt4XjKh0zTwvbxvv1r1jQUa55Y692_c/edit?usp=sharing
---
+++ js/video.js
@@ -0,0 +1,85 @@
+var video;
+var volume;
+var old;
+
+window.addEventListener("load", function() {
+	video = document.querySelector("#myVideo");
+	volume = document.querySelector("#volume");
+	console.log("Starting")
+	console.log(video.volume)
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	console.log(this)
+	video.play()
+	volume.innerHTML = (video.volume)*100 + "%"
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	console.log(this)
+	video.pause()
+});
+
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	console.log(this)
+	video.playbackRate=video.playbackRate*.9;
+	console.log(video.playbackRate)
+});
+
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up");
+	console.log(this)
+	video.playbackRate = video.playbackRate/.9;
+	console.log(video.playbackRate)
+});
+
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime += 5;
+	if(video.ended){
+		video.currentTime = 0;
+	}
+	console.log("Current location is " + video.currentTime);
+});
+
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted){
+		console.log("Unmuted")
+		video.muted = false;
+		document.querySelector("#mute").innerHTML="mute";
+	}
+	else{
+		console.log("Muted")
+		video.muted= true;
+		document.querySelector("#mute").innerHTML="Unmute"
+	}
+});
+
+
+document.querySelector("#volumeSlider").addEventListener("change", function() {
+	console.log("volumeSlider Video");
+	console.log(this)
+	console.log(this.value)
+	volume.innerHTML = this.value + "%"
+	video.volume = (this.value)/100
+});
+
+
+document.querySelector("#old").addEventListener("click", function() {
+	console.log("Old School");
+	console.log(this)
+	video.classList.add("oldTime")
+});
+
+
+document.querySelector("#original").addEventListener("click", function() {
+	console.log("Original Video");
+	console.log(this)
+	video.classList.remove("oldTime")
+});
